9b7d92cdcaf68b46b3bc8984abc6dea93dab4e4c
Shubham <shubhampatil1356@gmail.com>
2025-08-14 20:56:37 +0530

    Add firebase and react toastify setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Chat from "./pages/Chat/Chat";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <>
+      <ToastContainer />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
