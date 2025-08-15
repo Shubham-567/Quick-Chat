a32f65f10d1a49667ebb06f1d04ba696a191ba6f
Shubham <shubhampatil1356@gmail.com>
2025-08-15 23:45:18 +0530

    Backend: Create expres server

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,22 @@
-console.log("Hello, world!");
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+
+// create express app and http server
+const app = express();
+const server = http.createServer(app);
+
+// middleware support
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+app.use("/api/status", (req, res) => {
+  res.send("Server is live");
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server is running on port: " + PORT);
+});
