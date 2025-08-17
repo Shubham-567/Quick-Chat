02c89f052ac7420cd9de3e3aa9f2ba491c6881b5
Shubham <shubhampatil1356@gmail.com>
2025-08-18 00:30:42 +0530

    Frontend: Create AuthContext with check user and connect socket functions

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,19 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import Chat from "./pages/Chat/Chat";
 import ProfileUpdate from "./pages/ProfileUpdate/ProfileUpdate";
-import { ToastContainer, toast } from "react-toastify";
+import { AuthProvider } from "./context/AuthContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
-      <ToastContainer />
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Login />} />
-          <Route path='/chat' element={<Chat />} />
-          <Route path='/profile' element={<ProfileUpdate />} />
-          <Route path='*' element={<p>Not Found 404</p>} />
-        </Routes>
+        <AuthProvider>
+          <Toaster />
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/chat' element={<Chat />} />
+            <Route path='/profile' element={<ProfileUpdate />} />
+            <Route path='*' element={<p>Not Found 404</p>} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </>
   );
