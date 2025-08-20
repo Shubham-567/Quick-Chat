c6f4fab65a9a9eefa20a24b99f1eba3c3255178a
Shubham <shubhampatil1356@gmail.com>
2025-08-20 21:10:05 +0530

    Frontend: Add dynamic info and logout function to RightSidebar

--- Frontend/src/components/RightSidebar/RightSidebar.jsx
+++ Frontend/src/components/RightSidebar/RightSidebar.jsx
@@ -1,40 +1,52 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import assets from "../../assets/assets";
 import "./RightSidebar.css";
 import { ChatContext } from "../../context/ChatContext";
+import { AuthContext } from "../../context/AuthContext";
 
 const RightSidebar = () => {
-  const { selectedUser } = useContext(ChatContext);
+  const { selectedUser, messages } = useContext(ChatContext);
+  const { onlineUsers, logout } = useContext(AuthContext);
+  const [msgImages, setMsgImages] = useState([]);
+
+  useEffect(() => {
+    console.log(messages);
+    setMsgImages(messages.filter((msg) => msg.image).map((msg) => msg.image));
+  }, [messages]);
 
   return (
     <>
       {selectedUser && (
         <div className='rs'>
           <div className='rs-profile'>
-            <img src={assets.profile_img} alt='profile-img' />
+            <img
+              src={selectedUser?.profilePic || assets.avatar_icon}
+              alt='profile-img'
+            />
             <h3>
-              John Wick
-              <img
-                src={assets.green_dot}
-                alt='online-indicator'
-                className='dot'
-              />
+              {selectedUser.fullname}
+
+              {onlineUsers.includes(selectedUser._id) && (
+                <img
+                  src={assets.green_dot}
+                  alt='online-indicator'
+                  className='dot'
+                />
+              )}
             </h3>
-            <p>Hey, There i am John Wick using web chat app</p>
+            <p>{selectedUser.bio}</p>
           </div>
           <hr />
           <div className='rs-media'>
             <p>Media</p>
             <div>
-              <img src={assets.pic1} alt='media-1' />
-              <img src={assets.pic2} alt='media-2' />
-              <img src={assets.pic3} alt='media-3' />
-              <img src={assets.pic4} alt='media-4' />
-              <img src={assets.pic1} alt='media-5' />
-              <img src={assets.pic2} alt='media-6' />
+              {msgImages.length > 0 &&
+                msgImages.map((img, i) => (
+                  <img key={i} src={img} alt={`image-${i}`} />
+                ))}
             </div>
           </div>
-          <button>Logout</button>
+          <button onClick={logout}>Logout</button>
         </div>
       )}
     </>
